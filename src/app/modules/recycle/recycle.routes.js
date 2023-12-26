46a9a714d88b9c33997fcbd9bf31d8aec31999e6
israt-emu <isratjahan.emu1998@gmail.com>
2023-12-26 16:35:49 +0600

    reprint done

--- src/app/modules/recycle/recycle.routes.js
+++ src/app/modules/recycle/recycle.routes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post("/", validateRequest(createRecycleZodSchema), createRecycle);
 // router.post("/deleteProduct", validateRequest(addORDeleteProductToRecycleZodSchema), addOrDeleteProductToRecycle);
 router.get("/getAll", getAllRecycle);
-router.get("/getsingle/:id", getSingleRecycle);
+router.get("/getsingle", getSingleRecycle);
 router.post("/deleteRecycle", deleteRecycle);
 router.post("/updateStatus", updateRecycleStatus);
 router.post("/reprint", reprintProduct);
