6163d1cfb7ee8b7014eeaa5eff711fb8737a46c1
israt-emu <isratjahan.emu1998@gmail.com>
2023-12-31 09:26:37 +0600

    designer validation

--- src/app/modules/designerDetails/designerDetails.routes.js
+++ src/app/modules/designerDetails/designerDetails.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import {createDesignerDetails, deleteDesignerDetails, getDesignerDetails, updateDesignerDetails} from "./designerDetails.controller.js";
+import {addDesignerDetailsZodSchema} from "./designerDetails.validation.js";
+import {validateRequest} from "../../middlewars/validateRequest.js";
 const router = express.Router();
 
-router.post("/", createDesignerDetails);
+router.post("/", validateRequest(addDesignerDetailsZodSchema), createDesignerDetails);
 router.get("/getsingle/:id", getDesignerDetails);
 router.delete("/:id", deleteDesignerDetails);
 router.post("/updateDetails", updateDesignerDetails);
